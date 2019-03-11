77f0e4266cb0b2aff7e2abea792cf194e07a45e2
khanmohib09@gmail.com <mohib.khan@parker.com>
2019-03-11 13:55:56 -0400

    Token and Front end

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,22 +11,22 @@ export class RegisterComponent implements OnInit {
   // @Input() valuesFromHome: any; // To Get Values From Home this is set as Input
   @Output() cancelRegister = new EventEmitter(); // Ouput properties emit events
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration succesful');
+      this.alertify.success('Registration succesful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
+    // console.log('cancelled');
   }
 
 }
