0d3544a31e3aa5f3034fd6d99bd633bf3eb1d93e
khanmohib09@gmail.com <mohib.khan@parker.com>
2019-03-11 08:50:25 -0400

    Register and Login Service

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  // @Input() valuesFromHome: any; // To Get Values From Home this is set as Input
+  @Output() cancelRegister = new EventEmitter(); // Ouput properties emit events
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registration succesful');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+
+}
